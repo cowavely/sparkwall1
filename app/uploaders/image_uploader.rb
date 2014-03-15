61f28d35515d156528c962533c7e617ba3b3d9b8
Moe Sunami <17sunamim@gmail.com>
2014-03-15 15:16:05 +0900

    made sure files can be freaking saved to the website yoooo

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,6 +21,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
   
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
